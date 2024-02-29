f343f6b11527ac473074c7443d9bb6b78af1ecb6
mmalfoy <stpmalfoy@gmail.com>
2024-03-01 00:12:35 +0900

    두 용액 / 2시간 20분

--- mmalfoy/BOJ_2110.java
+++ mmalfoy/BOJ_2110.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-public class BOJ_2110 {
+public class BOJ_2470 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
