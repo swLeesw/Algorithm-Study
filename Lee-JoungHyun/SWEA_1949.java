7567f10b6d04e0304d7e3910f2638a7cfed24ff9
Lee-JoungHyun <98592001+DoublyH@users.noreply.github.com>
2024-03-03 08:56:57 +0900

    등산로 조성 / 180분

--- Lee-JoungHyun/SWEA_1949.java
+++ Lee-JoungHyun/SWEA_1949.java
@@ -54,7 +54,10 @@ public class SWEA_1949 {
 
 	private static int find(int N, int[][] top) {
 		//1. top별로 BFS 진행하며 max등산로 찾아주기!
+		
 		for (int i = 0; i < top.length; i++) {
+			visited = new boolean[N][N];
+			visited[top[i][0]][top[i][1]] = true;
 			DFS(top[i][0], top[i][1], false, 1);
 		}	
 		return answer;
@@ -78,6 +81,7 @@ public class SWEA_1949 {
 		
 	}
 	static int answer, map[][], K, N;
+	static boolean visited[][];
 	private static void DFS(int y, int x, boolean cutFlag, int length) {
 		int[] dx = { 0, 1, 0, -1 };
 		int[] dy = { 1, 0, -1, 0 };	
@@ -86,19 +90,65 @@ public class SWEA_1949 {
 			int ny = y + dy[d];
 			int nx = x + dx[d];
 			// 4방 그래프 확인 + 남은 자른 횟수 써서 잘랐을 떄 갈 수 있는지
-			if (0 <= ny && ny < N && 0 <= nx && nx < N) {
+			if (0 <= ny && ny < N && 0 <= nx && nx < N && !visited[ny][nx]) {
 				//System.out.println(y + " " + x + " - " + ny + " " + nx);
 				// 나보다 클때 공사 가능여부 확인
 				if (map[ny][nx] >= map[y][x] && !cutFlag && map[ny][nx] - map[y][x] < K) {
 					int tmp = map[ny][nx];
+					visited[ny][nx] = true;
 					map[ny][nx] = map[y][x]-1;
 					DFS(ny, nx, true, length+1);
+					visited[ny][nx] = false;
 					map[ny][nx] = tmp;
 				} else if (map[ny][nx] < map[y][x]) {
+					visited[ny][nx] = true;
 					DFS(ny, nx, cutFlag, length+1);
+					visited[ny][nx] = false;
 				}
 			}
 		}
 		
 	}
+	
+	
+
+//	private static int BFS(int[][] map, int y, int x, int K) {
+//		int N = map.length;
+//		// 해당 위치 온 cutCnt와 length를 저장
+//		int[] dx = { 0, 1, 0, -1 };
+//		int[] dy = { 1, 0, -1, 0 };		
+//		Queue<Poz> queue = new LinkedList<Poz>();
+//		queue.add(new Poz(y, x, map[y][x], 0));
+//		int depth = 1;
+//		Poz now;
+//		while (!queue.isEmpty()) {
+//			int qsize = queue.size();
+//			//System.out.println(depth);
+//			while (qsize-- > 0) {
+//				now = queue.poll();
+//				//System.out.println(now);
+//				for (int d = 0; d < 4; d++) {
+//					int ny = now.y + dy[d];
+//					int nx = now.x + dx[d];
+//					//  && map[ny][nx] - (K - now.cutCnt) < map[now.y][now.x]
+//					// 4방 그래프 확인 + 남은 자른 횟수 써서 잘랐을 떄 갈 수 있는지
+//					if (0 <= ny && ny < N && 0 <= nx && nx < N) {
+//						// 나보다 클때 공사 가능여부 확인
+//						if (map[ny][nx] >= now.hight && now.cutCnt == 0 && map[ny][nx] - now.hight < K ) {
+//							queue.add(new Poz(ny, nx, now.hight-1, 1));
+//						}
+//						else if (map[ny][nx] < now.hight) { 
+//							queue.add(new Poz(ny, nx, map[ny][nx], now.cutCnt));
+//						}
+//					}
+//				}
+//			}
+//			depth++;
+//		}
+//		return depth-1;
+//	}
+	
+	
+	
+
 }
