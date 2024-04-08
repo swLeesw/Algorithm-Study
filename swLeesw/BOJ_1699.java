4655cad595d5c9dca3df3c0064d7fff99a791130
SeungwonLee <74245930+swLeesw@users.noreply.github.com>
2024-04-08 23:36:16 +0900

    제곱수의 합 / 50분

---
+++ swLeesw/BOJ_1699.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1699 {
+
+	static int n, dp[];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		dp = new int[n + 1];
+		
+		for (int i = 1; i <= n; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j * j <= i; j++) {
+				dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+			}
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
+	
+}
